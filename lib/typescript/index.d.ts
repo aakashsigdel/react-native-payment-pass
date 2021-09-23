6cd95429757807389c5705de5c7491bed87c55b7
Aakash Sigdel <aakash@novus.world>
2021-09-23 15:31:20 +0100

    Build modules

--- lib/typescript/index.d.ts
+++ lib/typescript/index.d.ts
@@ -1,6 +1,17 @@
+export declare enum AddPaymentPassStatus {
+    CAN_ADD = "CAN_ADD",
+    ALREADY_ADDED = "ALREADY_ADDED",
+    BLOCKED = "BLOCKED"
+}
+export interface DigitalWalletProvisionRequestParams {
+    certificates: string[];
+    nonce: string;
+    nonceSignature: string;
+    appVersion: string;
+}
 declare type PaymentPassType = {
-    canAddPaymentPass: (paymentRefrenceId: string) => Promise<void>;
-    addPaymentPass: (cardHolderName: string, lastFour: string, paymentReferenceId: string) => Promise<void>;
+    canAddPaymentPass: (paymentRefrenceId: string) => Promise<AddPaymentPassStatus>;
+    addPaymentPass: (cardHolderName: string, lastFour: string, paymentReferenceId: string, errorCallback: () => void, successCallback: (params: DigitalWalletProvisionRequestParams) => void) => Promise<void>;
 };
 declare const _default: PaymentPassType;
 export default _default;
