f4d7d73b977f8ed961e27f4694114c0a8b7a126d
Aakash Sigdel <aakash@novus.world>
2021-09-23 18:38:28 +0100

    Build modules

--- lib/typescript/index.d.ts
+++ lib/typescript/index.d.ts
@@ -12,6 +12,7 @@ export interface DigitalWalletProvisionRequestParams {
 declare type PaymentPassType = {
     canAddPaymentPass: (paymentRefrenceId: string) => Promise<AddPaymentPassStatus>;
     addPaymentPass: (cardHolderName: string, lastFour: string, paymentReferenceId: string, errorCallback: () => void, successCallback: (params: DigitalWalletProvisionRequestParams) => void) => Promise<void>;
+    finalizeAddCard: (encryptedPassData: string, activationData: string, ephemeralPublicKey: string) => Promise<void>;
 };
 declare const _default: PaymentPassType;
 export default _default;
